9a78eef7a8fa13122223eb1dbbcdb86ed24f00ea
Md. Nazmus Sakib <sakib036@gmail.com>
2022-11-25 20:50:26 -0800

    get-by-mobile-id

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const app=express();
+const app = express();
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const port=process.env.PORT||5000;
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const port = process.env.PORT || 5000;
 
 
 app.use(cors());
@@ -14,25 +14,42 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.cez8utx.mongodb.net/?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-async function run(){
-    try{
+async function run() {
+    try {
         const mobilesCollection = client.db('ssh-mobile').collection('mobiles');
-       
-
         app.get('/mobiles', async (req, res) => {
-          
-           
-        const query = {};
-       
-      
-            const result =await mobilesCollection.find(query,).toArray();
+            const query = {};
+            const result = await mobilesCollection.find(query,).toArray();
             res.send(result)
-         
-           
+        });
+        app.get('/mobiles/:brand', async (req, res) => {
+            const brand = req.params.brand;
+            if(brand==="other"){
+                const query={};
+                const otherMobiles = await mobilesCollection.find(query,).toArray();
+
+                const result =otherMobiles.filter(otherMobile => otherMobile.brandName !== 'samsung' && otherMobile.brandName !== 'iphone');
+                res.send(result)
+            }
+            else{
+                const query={
+                    brandName:brand
+                }
+                const result = await mobilesCollection.find(query,).toArray();
+                res.send(result)
+            }
+          
+        });
+
+        app.get('/mobiles/:brand/:id', async (req, res) => {
+            const id=req.params.id;
+            const query={_id:ObjectId(id)}
+            const result = await mobilesCollection.findOne(query);
+            res.send(result)
         });
 
     }
-    finally{
+    finally {
 
     }
 }
@@ -43,11 +60,11 @@ run().catch(console.log)
 
 
 
-app.get('/',(req,res)=>{
+app.get('/', (req, res) => {
     res.send('SSH-MOBILE SERVER IS RUNNING');
 });
 
 
-app.listen(port,()=>{
+app.listen(port, () => {
     console.log(`SSH-MOBILE server running on Port ${port}`)
 })
