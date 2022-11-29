5258174d5e35503d320def8c8c6155add7927535
Md. Nazmus Sakib <sakib036@gmail.com>
2022-11-28 18:31:12 -0800

    payment

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 require('dotenv').config();
 const app = express();
+const stripe = require("stripe")(process.env.STRIPE_KEY);
+
 
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
@@ -13,12 +16,33 @@ app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.cez8utx.mongodb.net/?retryWrites=true&w=majority`;
 
+
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send('unauthorized access')
+    }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_KEY, function (err, decoded) {
+        if (err) {
+            return res.status(403).send({ message: 'forbidden access' })
+        }
+        req.decoded = decoded;
+        next();
+    })
+}
+
+
+
+
 async function run() {
     try {
         const mobilesCollection = client.db('ssh-mobile').collection('mobiles');
         const bookingsCollection = client.db('ssh-mobile').collection('bookings');
         const usersCollection = client.db('ssh-mobile').collection('users');
+        const paymentsCollection = client.db('ssh-mobile').collection('payments');
 
 
         app.get('/mobiles', async (req, res) => {
@@ -36,6 +60,8 @@ async function run() {
         });
         app.get('/mobiles/:brand', async (req, res) => {
             const query = {};
+           
+            
             const allPhones = await mobilesCollection.find(query).toArray();
 
             const bookingQuery = {};
@@ -91,6 +117,8 @@ async function run() {
 
        
 
+       
+
 
         app.get('/bookings', async (req, res) => {
             const email = req.query.email;
@@ -102,6 +130,16 @@ async function run() {
             const bookings = await bookingsCollection.find(query).toArray();
             res.send(bookings)
         });
+
+
+        app.get('/bookings/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) }
+            const result = await bookingsCollection.findOne(query);
+            res.send(result);
+        });
+
+
 
         app.get('/mobiles/dashboard/myProduct/:email', async(req,res)=>{
             const email=req.params.email;
@@ -150,13 +188,63 @@ async function run() {
         }
         const result=await usersCollection.findOne(query);
         res.send(result)
-    })
+    });
 
-        app.post('/mobiles', async (req, res) => {
+    app.get('/jwt', async (req, res) => {
+        const email = req.query.email;
+       
+        const query = {
+            userEmail: email
+        }
+        const user = await usersCollection.findOne(query);
+        if (user) {
+            const token = jwt.sign({ email }, process.env.ACCESS_TOKEN_KEY, { expiresIn: '1d' })
+            return res.send({ accessToken: token })
+        }
+        res.status(403).send({ accessToken: '' })
+    });
+
+        app.post('/mobiles',verifyJWT, async (req, res) => {
             const mobile = req.body;
             const result = await mobilesCollection.insertOne(mobile);
             res.send(result)
         });
+
+
+        app.post("/create-payment-intent", async (req, res) => {
+            const order=req.body
+            console.log(order)
+            const price=order.mobilePrice
+            const amount=price*100;
+
+            const paymentIntent = await stripe.paymentIntents.create({
+                currency:'usd',
+                amount:amount,
+                "payment_method_types":[
+                    'card'
+                ]
+            });
+            res.send({
+                clientSecret: paymentIntent.client_secret,
+              });
+           
+        })
+
+        app.post('/payments', async(req,res)=>{
+            const payment=req.body;
+            const result= await paymentsCollection.insertOne(payment);
+            const id=payment.bookingId;
+            const filter={_id:ObjectId(id)}
+
+            const updateDoc={
+                $set:{
+                    paid:true,
+                    transactionId:payment.transactionId,
+                }
+            }
+            const updatedResult=await bookingsCollection.updateOne(filter,updateDoc)
+            res.send(result)
+        })
 
 
         app.post('/bookings', async (req, res) => {
@@ -203,7 +291,7 @@ async function run() {
         });
 
 
-        app.put('/user/:id',  async (req, res) => {
+        app.put('/user/:id',verifyJWT,  async (req, res) => {
 
             const id = req.params.id;
             const filter = { _id: ObjectId(id) };
@@ -218,19 +306,19 @@ async function run() {
             res.send(result)
         });
 
-        app.delete('/mobiles/:id', async (req, res) => {
+        app.delete('/mobiles/:id', verifyJWT, async (req, res) => {
             const id = req.params.id;
             const query = { _id: ObjectId(id) };
             const result = await mobilesCollection.deleteOne(query)
             res.send(result)
         })
-        app.delete('/bookings/:id', async (req, res) => {
+        app.delete('/bookings/:id',verifyJWT, async (req, res) => {
             const id = req.params.id;
             const query = { _id: ObjectId(id) };
             const result = await bookingsCollection.deleteOne(query)
             res.send(result)
         })
-        app.delete('/users/:id', async (req, res) => {
+        app.delete('/users/:id',verifyJWT, async (req, res) => {
             const id = req.params.id;
             const query = { _id: ObjectId(id) };
             const result = await usersCollection.deleteOne(query)
